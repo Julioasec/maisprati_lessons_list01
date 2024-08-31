af3b21f26b2bd5a7a92b4953528a514f88ee1d4a
JulioAsecEd <ed_test_julio@outlook.com>
2024-08-31 08:05:59 -0300

    lesson03

---
+++ lessons/lesson03.js
@@ -0,0 +1,18 @@
+let prompt = require('prompt-sync')() // Chamada do método prompt-sync
+
+let grade = Number(prompt("Informe uma nota: ")) // recebe a nota do usuário
+
+if (grade > 10 || grade < 0) {
+     return console.log("Nota inválida");
+    
+}
+
+if(grade >= 7){
+    return console.log("Aprovado!");
+    
+}else if(grade <= 6 && grade >= 4){
+  return  console.log("Recuperação!");
+    
+}
+
+return console.log("Reprovado!");
