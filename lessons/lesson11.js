d9226a936bd5d9a0daf978ee6780b2ec839dfedb
JulioAsecEd <ed_test_julio@outlook.com>
2024-08-31 21:34:48 -0300

    lesson11

---
+++ lessons/lesson11.js
@@ -0,0 +1,11 @@
+let prompt = require('prompt-sync')() // Chamada do método prompt-sync
+
+let sum = 0;
+
+
+for(let i = 0; i < 5; i++){
+    sum += Number(prompt(`Informe o número ${i+1}`))
+
+}
+
+console.log("A soma é igual a : " + sum );
